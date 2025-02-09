4cc47559a6c87ab5a872210a3c0c5fe7faaf44d5
Luca Battesini <lucabattesini1@gmail.com>
2025-02-09 14:40:46 -0300

    Recorrectng the error and using the correct cmmand in the correct page

--- first-next-project/app/page.js
+++ first-next-project/app/page.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Welcome to the main page</h1>
       <p>Ths is the main page</p>
-      <p><a href="/about">About Us</a></p>
+      <p><Link href="/about">About Us</Link></p>
     </main>
   );
 }
