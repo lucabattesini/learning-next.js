d96a07caa47a4101a8b6accf1d04892058f7f7ff
Luca Battesini <lucabattesini1@gmail.com>
2025-02-09 15:15:26 -0300

    Just aking some notes

--- first-next-project/app/layout.js
+++ first-next-project/app/layout.js
@@ -1,10 +1,12 @@
 import "./globals.css";
 
+//All the contet that goes to <head> is set with 'metadata'
 export const metadata = {
   title: 'My NextJS project',
   description: 'Nothing to say'
 };
 
+//Children is the content in the page
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
